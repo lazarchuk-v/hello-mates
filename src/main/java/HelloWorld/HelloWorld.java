e2a19697793e143d281da872181ed80abe7c0ac1
Volodymyr Lazarchuk <lazarchuk.v.u@gmail.com>
2023-06-14 11:09:19 +0300

    Initial commit

---
+++ src/main/java/HelloWorld/HelloWorld.java
@@ -0,0 +1,10 @@
+package HelloWorld;
+
+/**
+ * Created by speci on 23/06/14.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
